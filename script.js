679599047f89f531f736bfbd07791556db9b48a2
Rlter-glitch <noprlter@gmail.com>
2025-03-11 09:06:15 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,500 @@
+const questions = [
+  {
+    question: "Câu 1: Trong các thế kỷ XVI – XIX, phong trào đấu tranh chống thực dân xâm lược bùng nổ sớm ở những quốc gia Đông Nam Á nào sau đây?",
+    options: ["a) In-đô-nê-xi-a và Miến Điện.", "b) In-đô-nê-xi-a và Phi-líp-pin.", "c) Việt Nam và Lào.", "d) Mã Lai và Phi-líp-pin."],
+    correct: "b"
+  },
+  {
+    question: "Câu 2: Từ thế kỉ XVI đến thế kỉ XIX, nhân dân In-đô-nê-xi-a đấu tranh chống lại nước thực dân nào sau đây?",
+    options: ["a) Hà Lan.", "b) Tây Ban Nha.", "c) Anh.", "d) Pháp."],
+    correct: "a"
+  },
+  {
+    question: "Câu 3: Từ giữa thế kỷ XVI đến cuối thế kỷ XIX, nhân dân Phi-líp-pin đấu tranh chống lại nước thực dân nào sau đây?",
+    options: ["a) Tây Ban Nha.", "b) Bồ Đào Nha.", "c) Mĩ.", "d) Hà Lan."],
+    correct: "a"
+  },
+  {
+    question: "Câu 4: Năm 1945, quốc gia giành độc lập sớm nhất ở Đông Nam Á là",
+    options: ["a) In-đô-nê-xi-a.", "b) Lào.", "c) Campuchia.", "d) Việt Nam."],
+    correct: "a"
+  },
+  {
+    question: "Câu 5: Từ nửa sau thế kỉ XIX, các nước Đông Dương trở thành thuộc địa của nước thực dân nào sau đây?",
+    options: ["a) Anh.", "b) Pháp.", "c) Hà Lan.", "d) Tây Ban Nha."],
+    correct: "b"
+  },
+  {
+    question: "Câu 7: Năm 1858, Pháp tiến hành xâm lược Việt Nam bằng kế hoạch chiến tranh nào sau đây?",
+    options: ["a) \"Đánh nhanh, thắng nhanh\".", "b) \"Chinh phục từng gói nhỏ\".", "c) \"Tràn ngập lãnh thổ\".", "d) \"Chiến tranh tổng lực\"."],
+    correct: "a"
+  },
+  {
+    question: "Câu 8: Cuộc khởi nghĩa của nhân dân Campuchia chống thực dân Pháp trong những năm 1863-1866 do ai lãnh đạo?",
+    options: ["a) Pu-côm-bô.", "b) A-cha Xoa.", "c) Com-ma-đam.", "d) Si-vô-tha."],
+    correct: "b"
+  },
+  {
+    question: "Câu 9: Cuối thế kỉ XIX, ở Đông Nam Á phong trào đấu tranh theo ý thức hệ phong kiến dần được thay thế bằng phong trào đấu tranh theo xu hướng nào sau đây?",
+    options: ["a) Vô sản.", "b) Tư sản.", "c) Li khai.", "d) Cực đoan."],
+    correct: "a"
+  },
+  {
+    question: "Câu 10: Sau Chiến tranh thế giới thứ nhất, tại Đông Nam Á, Đảng Cộng sản nước nào được thành lập sớm nhất?",
+    options: ["a) Việt Nam.", "b) Mã Lai.", "c) Phi-líp-pin.", "d) In-đô-nê-xi-a."],
+    correct: "d"
+  },
+  {
+    question: "Câu 11: Trong những năm 1930-1951, cách mạng Đông Dương đặt dưới sự lãnh đạo của",
+    options: ["a) Đảng cộng sản Việt Nam.", "b) Đảng Lao động Việt Nam.", "c) Đảng cộng sản Đông Dương.", "d) Đảng Nhân dân cách mạng Lào."],
+    correct: "c"
+  },
+  {
+    question: "Câu 12: Năm 1945, những quốc gia nào ở Đông Nam Á tuyên bố độc lập?",
+    options: ["a) In-đô-nê-xi-a, Việt Nam, Lào.", "b) In-đô-nê-xi-a, Việt Nam, Cam-pu-chia.", "c) In-đô-nê-xi-a, Việt Nam, Phi-lip-pin.", "d) In-đô-nê-xi-a, Việt Nam, Ma-lai-xi-a."],
+    correct: "a"
+  },
+    {
+    question: "Câu 13: Trong giai đoạn 1954-1975, nhân dân ba nước Đông Dương đấu tranh chống kẻ thù chung là",
+    options: ["a) thực dân Pháp.", "b) đế quốc Mĩ.", "c) thực dân Anh.", "d) thực dân Hà Lan."],
+    correct: "b"
+  },
+  {
+    question: "Câu 14: Sau Chiến tranh thế giới thứ hai, những quốc gia nào ở Đông Nam Á giành và tuyên bố độc lập dưới sự lãnh đạo của Đảng Cộng sản?",
+    options: ["a) Việt Nam và Lào.", "b) Lào và In-đô-nê-xi-a.", "c) Việt Nam, Lào và In-đô-nê-xi-a.", "d) Mianma, Việt Nam, Malaixia."],
+    correct: "c"
+  },
+  {
+    question: "Câu 15: Trước khi giành độc lập, Xin-ga-po là thuộc địa của nước nào sau đây?",
+    options: ["a) Mĩ.", "b) Hà Lan.", "c) Pháp.", "d) Anh."],
+    correct: "d"
+  },
+  {
+    question: "Câu 16: Trước chiến tranh thế giới thứ hai (1939-1945), các nước Việt Nam, Lào, Campuchia đều là thuộc địa của",
+    options: ["a) Pháp.", "b) Mĩ.", "c) Anh.", "d) Hà Lan."],
+    correct: "a"
+  },
+  {
+    question: "Câu 17: Sau chiến tranh thế giới thứ hai, một số nước Đông Nam Á bắt đầu quá trình tái thiết đất nước nhằm mục đích nào sau đây?",
+    options: ["a) Khắc phục hậu quả chiến tranh và các tàn dư của thời kì thuộc địa.", "b) Liên minh với các nước phương Tây để tranh thủ viện trợ.", "c) Nỗ lực trở thành ủy viên thường trực của Hội đồng bảo an.", "d) Đưa đất nước phát triển thành các cường quốc quân sự hàng đầu."],
+    correct: "a"
+  },
+  {
+    question: "Câu 18: Thời kì đầu sau khi độc lập, nhóm các nước sáng lập ASEAN đã thực hiện chính sách phát triển nào sau đây?",
+    options: ["a) Công nghiệp hóa thay thế nhập khẩu.", "b) Công nghiệp hóa hướng tới xuất khẩu.", "c) Công nghiệp hóa xã hội chủ nghĩa.", "d) Cải cách và mở cửa."],
+    correct: "a"
+  },
+  {
+    question: "Câu 19: Trong những năm 70 của thế kỉ XX, nhóm 5 nước sáng lập ASEAN đã thực hiện chính sách kinh tế nào sau đây?",
+    options: ["a) Công nghiệp hóa hướng tới xuất khẩu.", "b) Chiến lược kinh tế hướng nội.", "c) Xây dựng kinh tế thị trường.", "d) Chính sách láng giềng thân thiện."],
+    correct: "a"
+  },
+  {
+    question: "Câu 20: Tháng 12-1986, Đảng cộng sản Việt Nam đã quyết định thực hiện",
+    options: ["a) đường lối đổi mới.", "b) xây dựng nền kinh tế tập trung.", "c) trưng thu lương thực thừa.", "d) chính sách bế quan tỏa cảng."],
+    correct: "a"
+  },
+  {
+    question: "Câu 21: Chiến tranh bảo vệ Tổ quốc có vai trò quan trọng",
+    options: [
+      "a) trong việc hình thành và nâng cao lòng tự hào dân tộc.",
+      "b) trong việc hình thành và phát triển truyền thống yêu nước.",
+      "c) đối với sự sinh tồn và phát triển của dân tộc Việt Nam.",
+      "d) khơi dậy và củng cố tinh thần đoàn kết, lòng tự hào dân tộc."
+    ],
+    correct: "c"
+  },
+  {
+    question: "Câu 22: Trận quyết chiến trong cuộc kháng chiến chống quân xâm lược Nam Hán năm 938 là trận",
+    options: [
+      "a) Bạch Đằng.",
+      "b) Như Nguyệt.",
+      "c) Bình Lệ Nguyên.",
+      "d) Rạch Gầm - Xoài Mút."
+    ],
+    correct: "a"
+  },
+  {
+    question: "Câu 23: Trận quyết chiến trong cuộc kháng chiến chống quân xâm lược Tống thời Lý là",
+    options: [
+      "a) trận Bạch Đằng.",
+      "b) trận Như Nguyệt.",
+      "c) trận Ngọc Hồi - Đống Đa.",
+      "d) trận Rạch Gầm - Xoài Mút."
+    ],
+    correct: "b"
+  },
+  {
+    question: "Câu 24: Trận quyết chiến trong cuộc kháng chiến chống quân xâm lược Thanh năm 1789 là",
+    options: [
+      "a) trận Bạch Đằng.",
+      "b) trận Như Nguyệt.",
+      "c) trận Ngọc Hồi - Đống Đa.",
+      "d) trận Rạch Gầm - Xoài Mút."
+    ],
+    correct: "c"
+  },
+  {
+    question: "Câu 25: Lãnh đạo cuộc kháng chiến chống quân xâm lược Nam Hán năm 938 là",
+    options: [
+      "a) Ngô Quyền.",
+      "b) Trần Hưng Đạo.",
+      "c) Lê Lợi.",
+      "d) Quang Trung - Nguyễn Huệ."
+    ],
+    correct: "a"
+  },
+  {
+    question: "Câu 26: Lãnh đạo cuộc kháng chiến chống quân xâm lược Xiêm và Thanh cuối thế kỉ XVIII là",
+    options: [
+      "a) Ngô Quyền.",
+      "b) Trần Hưng Đạo.",
+      "c) Lê Lợi.",
+      "d) Nguyễn Huệ - Quang Trung."
+    ],
+    correct: "d"
+  },
+  {
+    question: "Câu 27: Cuộc kháng chiến chống quân Triệu xâm lược năm 179 TCN thất bại đã dẫn đến sự sụp đổ của nhà nước",
+    options: [
+      "a) Văn Lang.",
+      "b) Âu Lạc.",
+      "c) Vạn Xuân.",
+      "d) Nam Việt."
+    ],
+    correct: "b"
+  },
+  {
+    question: "Câu 28: Các cuộc kháng chiến chống ngoại xâm trong lịch sử dân tộc Việt Nam thắng lợi là do",
+    options: [
+      "a) kẻ thù chủ quan, không có tổ chức chặt chẽ.",
+      "b) tương quan lực lượng chênh lệch, địch có quân số ít hơn ta.",
+      "c) địch thiếu những tướng chỉ huy tài năng, nhiều kinh nghiệm.",
+      "d) Nhân dân Việt Nam yêu nước, đoàn kết kháng chiến."
+    ],
+    correct: "d"
+  },
+  {
+    question: "Câu 29: Một trong những cuộc kháng chiến không thành công của dân tộc Việt Nam trước Cách mạng tháng Tám năm 1945 là",
+    options: [
+      "a) kháng chiến chống Thanh của nhà Tây Sơn (1789).",
+      "b) kháng chiến chống quân Tống thời thời Lý (1075-1077).",
+      "c) kháng chiến chống thực dân Pháp của nhà Nguyễn (1858 - 1884).",
+      "d) kháng chiến chống quân Nam Hán của Ngô Quyền (938)."
+    ],
+    correct: "c"
+  },
+  {
+    question: "Câu 30: Một trong những nguyên nhân khiến trong suốt chiều dài lịch sử, dân tộc Việt Nam phải luôn đối phó với nhiều thế lực ngoại xâm để bảo vệ nền độc lập dân tộc là gì?",
+    options: [
+      "a) Việt Nam là cầu nối giữa châu Á, Âu, và Thái Bình Dương.",
+      "b) Việt Nam là cầu nối giữa châu Á và Thái Bình Dương.",
+      "c) Việt Nam có đường giao thương với hầu hết các nước trên biển Đông.",
+      "d) Việt Nam là cầu nối giữa Đông Nam Á lục địa và Đông Nam Á hải đảo."
+    ],
+    correct: "d"
+  },
+  {
+    question: "Câu 31: Ai là người lãnh đạo cuộc kháng chiến chống Tống trên sông Bạch Đằng?",
+    options: [
+      "a) Lý Thường Kiệt.",
+      "b) Lê Hoàn.",
+      "c) Ngô Quyền.",
+      "d) Trần Quốc Tuấn."
+    ],
+    correct: "b"
+  },
+  {
+    question: "Câu 32: Năm 1075 - 1077, Lý Thường Kiệt đánh tan quân Tống ở phòng tuyến trên dòng sông",
+    options: [
+      "a) Bạch Đằng.",
+      "b) Rạch Gầm.",
+      "c) Thu Bồn.",
+      "d) Như Nguyệt."
+    ],
+    correct: "d"
+  },
+  {
+    question: "Câu 33: Trận đánh tiêu biểu của quân dân nhà Trần trong cuộc kháng chiến chống quân Mông Cổ xâm lược (năm 1258) là",
+    options: [
+      "a) Đông Bộ Đầu.",
+      "b) sông Bạch Đằng.",
+      "c) Chi Lăng - Xương Giang.",
+      "d) Đống Đa."
+    ],
+    correct: "a"
+  },
+  {
+    question: "Câu 34: Những trận đánh tiêu biểu trong cuộc kháng chiến chống quân Nguyên xâm lược (năm 1285) là",
+    options: [
+      "a) Tây Kết, Hàm Tử, Bạch Đằng.",
+      "b) Tây Kết, Hàm Tử, Như Nguyệt.",
+      "c) Tây Kết, Hàm Tử, Chương Dương.",
+      "d) Chương Dương, Hàm Tử, Đống Đa."
+    ],
+    correct: "c"
+  },
+  {
+    question: "Câu 35: Vị tướng nào đã chỉ huy trận Bạch Đằng năm 1288 chống quân xâm lược Nguyên?",
+    options: [
+      "a) Trần Thái Tông.",
+      "b) Trần Nhân Tông.",
+      "c) Trần Thủ Độ.",
+      "d) Trần Quốc Tuấn."
+    ],
+    correct: "d"
+  },
+  {
+    question: "Câu 36: Trận đánh tiêu biểu trong cuộc kháng chiến chống quân Xiêm xâm lược (năm 1785) là",
+    options: [
+      "a) trận Rạch Gầm - Xoài Mút.",
+      "b) trận Bạch Đằng.",
+      "c) trận Như Nguyệt.",
+      "d) trận Ngọc hồi – Đống Đa."
+    ],
+    correct: "a"
+  },
+  {
+    question: "Câu 37: Trận Ngọc Hồi - Đống Đa là trận đánh tiêu biểu của quân và dân ta trong cuộc kháng chiến chống lại quân xâm lược",
+    options: [
+      "a) Minh.",
+      "b) Thanh.",
+      "c) Nguyên.",
+      "d) Xiêm."
+    ],
+    correct: "b"
+  },
+  {
+    question: "Câu 38: Kế sách “Tiên phát chế nhân” đã được triều đại nào sử dụng để chống lại quân xâm lược nào",
+    options: [
+      "a) Tiền Lê - quân Tống.",
+      "b) Nhà Lý - quân Tống.",
+      "c) Nhà Trần - quân Nguyên.",
+      "d) Hậu Lê - quân Minh."
+    ],
+    correct: "b"
+  },
+  {
+    question: "Câu 39: Trong cuộc kháng chiến chống quân xâm lược Thanh, vua Quang Trung đã sử dụng chiến thuật quân sự nào?",
+    options: [
+      "a) Thần tốc, bất ngờ.",
+      "b) Vườn không nhà trống.",
+      "c) Tiên phát chế nhân.",
+      "d) Đóng cọc trên sông."
+    ],
+    correct: "a"
+  },
+  {
+    question: "Câu 40: Câu nói: “Vua tôi đồng tâm, anh em hoà mục, cả nước góp sức, giặc phải bị bắt…” của Trần Quốc Tuấn đã đề cập đến nguyên nhân nào dẫn đến thắng lợi của quân dân nhà Trần?",
+    options: [
+      "a) Tính chính nghĩa của kháng chiến.",
+      "b) Kế sách đánh giặc đúng đắn.",
+      "c) Tinh thần đoàn kết của quân dân ta.",
+      "d) Kẻ địch gặp khó khăn."
+    ],
+    correct: "c"
+  },
+  {
+    question: "Câu 41: Trong xã hội nước ta dưới thời Bắc thuộc mâu thuẫn cơ bản trong xã hội nào sau đây là mâu thuẫn?",
+    options: [
+      "a) Giữa giai cấp nông dân với địa chủ phong kiến.",
+      "b) Giữa nhân dân ta với chính quyền đô hộ phương Bắc.",
+      "c) Giữa quý tộc với chính quyền đô hộ phương Bắc.",
+      "d) Giữa nông dân với chính quyền đô hộ phương Bắc."
+    ],
+    correct: "b"
+  },
+  {
+    question: "Câu 42: Năm 544, khởi nghĩa của Lí Bí thắng lợi. Sau khi lên làm vua, Lí Bí đặt quốc hiệu nước ta là?",
+    options: [
+      "a) Đại Việt.",
+      "b) Nam Việt.",
+      "c) Vạn Xuân.",
+      "d) Đại Cồ Việt."
+    ],
+    correct: "c"
+  },
+  {
+    question: "Câu 43: Cuộc khởi nghĩa đầu tiên của nhân dân Âu Lạc chống lại ách đô hộ của phong kiến phương Bắc là?",
+    options: [
+      "a) Khởi nghĩa Bà Triệu.",
+      "b) Khởi nghĩa Lý Bí.",
+      "c) Khởi nghĩa Hai Bà Trưng.",
+      "d) Khởi nghĩa Phùng Hưng."
+    ],
+    correct: "c"
+  },
+  {
+    question: "Câu 44: Khởi nghĩa của Hai Bà Trưng đã đánh bại quân xâm lược nào sau đây?",
+    options: [
+      "a) Nhà Hán.",
+      "b) Nhà Tùy.",
+      "c) Nhà Ngô.",
+      "d) Nhà Lương."
+    ],
+    correct: "a"
+  },
+  {
+    question: "Câu 45: Chiến thắng nào sau đây đã chấm dứt hoàn toàn thời kì một nghìn năm đô hộ của phong kiến phương Bắc đối với nước ta?",
+    options: [
+      "a) Chiến thắng tại phòng tuyến sông Như Nguyệt.",
+      "b) Chiến thắng trên sông Bạch Đằng.",
+      "c) Chiến thắng Chi Lăng – Xương Giang.",
+      "d) Chiến thắng Ngọc Hồi – Đống Đa."
+    ],
+    correct: "b"
+  },
+  {
+    question: "Câu 46: Ai là người lãnh đạo thành công cuộc khởi nghĩa đánh đuổi quân xâm lược nhà Đường năm 722 và được nhân dân suy tôn là “Bố cái đại vương”?",
+    options: [
+      "a) Mai Thúc Loan.",
+      "b) Phùng Hưng.",
+      "c) Bà Triệu.",
+      "d) Dương Đình Nghệ."
+    ],
+    correct: "b"
+  },
+  {
+    question: "Câu 47: “Tôi chỉ muốn cưỡi cơn gió mạnh, đạp luồng sóng dữ, chém cá kình ở biển Đông, đánh đuổi quân Ngô, giành lại giang san, cởi ách nô lệ, há chịu cúi đầu làm tì thiếp cho người ta!”. Câu nói trên là của nhân vật nào sau đây?",
+    options: [
+      "a) Bà Triệu (Triệu Thị Trinh).",
+      "b) Bà Trưng Trắc.",
+      "c) Nữ tướng Bùi Thị Xuân.",
+      "d) Công chúa Lê Ngọc Hân."
+    ],
+    correct: "a"
+  },
+  {
+    question: "Câu 48: Hai Bà Trưng khởi nghĩa nhằm mục đích gì?",
+    options: [
+      "a) Trả thù cho Thi Sách.",
+      "b) Trả thù nhà, đền nợ nước.",
+      "c) Rửa hận.",
+      "d) Trả thù riêng."
+    ],
+    correct: "b"
+  },
+  {
+    question: "Câu 49: Bà Triệu có tên thật là gì?",
+    options: [
+      "a) Triệu Thị Trinh.",
+      "b) Triệu Thiên Thư.",
+      "c) Triệu Thị Trang.",
+      "d) Triệu Thị Trắc."
+    ],
+    correct: "a"
+  },
+  {
+    question: "Câu 50: Năm 544, Lý Bí lên ngôi Hoàng đế, sử cũ gọi là?",
+    options: [
+      "a) Lý Bắc Đế.",
+      "b) Lý Nam Đế.",
+      "c) Lý Đông Đế.",
+      "d) Lý Tây Đế."
+    ],
+    correct: "b"
+  },
+  {
+    question: "Câu 51: Cuộc khởi nghĩa Hai Bà Trưng là cuộc khởi nghĩa thứ mấy trong thời Bắc thuộc?",
+    options: [
+      "a) Khởi nghĩa đầu tiên.",
+      "b) Khởi nghĩa thứ hai.",
+      "c) Khởi nghĩa thứ chín.",
+      "d) Khởi nghĩa cuối cùng."
+    ],
+    correct: "a"
+  },
+  {
+    question: "Câu 52: Cuộc khởi nghĩa nào sau đây không nằm trong phong trào đấu tranh chống sự đô hộ của các triều đại phong kiến phương Bắc?",
+    options: [
+      "a) Khởi nghĩa Hai Bà Trưng.",
+      "b) Khởi nghĩa Lam Sơn.",
+      "c) Phong trào Tây Sơn.",
+      "d) Khởi nghĩa Phùng Hưng."
+    ],
+    correct: "c"
+  },
+  {
+    question: "Câu 53: Năm 40 - 43 là thời gian diễn ra cuộc khởi nghĩa nào của nhân dân ta thời Bắc thuộc?",
+    options: [
+      "a) Khởi nghĩa Bà Triệu.",
+      "b) Khởi nghĩa Lý Bí.",
+      "c) Khởi nghĩa Hai Bà Trưng.",
+      "d) Khởi nghĩa Phùng Hưng."
+    ],
+    correct: "c"
+  },
+  {
+    question: "Câu 54: Năm 248, cuộc khởi nghĩa của Bà Triệu nhằm chống lại ách đô hộ của triều đại phong kiến phương Bắc nào?",
+    options: [
+      "a) Nhà Hán.",
+      "b) Nhà Ngô.",
+      "c) Nhà Đường.",
+      "d) Nhà Minh."
+    ],
+    correct: "a"
+  },
+  {
+    question: "Câu 55: Cuộc khởi nghĩa của Bà Triệu (năm 248) đã?",
+    options: [
+      "a) Mở đầu cuộc đấu tranh giành độc lập của nhân dân ta thời Bắc thuộc.",
+      "b) Tiếp tục khẳng định sức mạnh và ý chí của phụ nữ Việt Nam.",
+      "c) Mở ra thời kì độc lập, tự chủ lâu dài trong lịch sử dân tộc.",
+      "d) Tạo cơ sở cho sự thắng lợi hoàn toàn của cuộc đấu tranh giành độc lập về sau."
+    ],
+    correct: "b"
+  }
+];
+
+// Hàm để chèn câu hỏi và các lựa chọn vào form quiz
+function loadQuiz() {
+  const form = document.getElementById('quizForm');
+  questions.forEach((question, index) => {
+    console.log(question); // In ra để kiểm tra
+    const questionDiv = document.createElement('div');
+    questionDiv.classList.add('question');
+    
+    const questionText = document.createElement('p');
+    questionText.innerText = question.question;
+    questionDiv.appendChild(questionText);
+
+    question.options.forEach((option, i) => {
+      const label = document.createElement('label');
+      const input = document.createElement('input');
+      input.type = 'radio';
+      input.name = 'q' + (index + 1);
+      input.value = String.fromCharCode(97 + i); // 'a', 'b', 'c', 'd'
+      label.appendChild(input);
+      label.appendChild(document.createTextNode(option));
+      questionDiv.appendChild(label);
+      questionDiv.appendChild(document.createElement('br'));
+    });
+
+    form.appendChild(questionDiv);
+  });
+}
+
+// Hàm tính điểm và hiển thị kết quả khi người dùng nộp bài
+function submitQuiz() {
+  const form = document.getElementById('quizForm');
+  const questionsDivs = form.querySelectorAll('.question');
+  let score = 0;
+
+  questionsDivs.forEach((questionDiv, index) => {
+    const selectedOption = questionDiv.querySelector('input[type="radio"]:checked');
+    const correctAnswer = questions[index].correct;
+
+    if (selectedOption && selectedOption.value === correctAnswer) {
+      score++;
+      questionDiv.classList.add('correct');
+    } else {
+      questionDiv.classList.add('incorrect');
+    }
+  });
+
+  const resultDiv = document.getElementById('result');
+  resultDiv.innerHTML = `Bạn đã trả lời đúng ${score} trên ${questions.length} câu.`;
+}
+
+// Gọi hàm loadQuiz để tạo câu hỏi khi trang được tải
+loadQuiz();
